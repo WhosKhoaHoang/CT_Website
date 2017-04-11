3364fc64899816319da4cfb11829e70b4201431b
Khoa Hoang <whoskhoahoang@gmail.com>
2017-04-11 14:41:32 -0700

    Added some debugging echo statements.

--- index.php
+++ index.php
@@ -7,16 +7,20 @@
             //what's written in .htaccess. Think: it makes sense for this to happen, since an index is something
             //that would be used to navigate the contents of something -- e.g., a website!!!
             
+            //All the following is for if the user wants to go to a particular page directly from the address bar
             $uriGetParam = isset($_GET["uri"])? "/".$_GET["uri"] : "/";
             if ($uriGetParam == "/") {
+                echo("IN HOME!!!!!");
                 include("views/home.php");
                 
             }
             else if ($uriGetParam == "/services") {
+                echo("IN SERVICES!!!!!");
                 include("views/services.html");
                 
             }
             else if ($uriGetParam == "/stayclever"){
+                echo("IN STAY CLEVER!!!!!");
                 include("views/stay_clever.html");
             }
             
